928b0b3c62d9e915d63dbd7518fc0cf5ff99f1d7
litao.lt <litao.lt@alibaba-inc.com>
2014-02-17 16:53:05 +0800

    sector

--- canvax/shape/Sector.js
+++ canvax/shape/Sector.js
@@ -39,7 +39,6 @@ KISSY.add("canvax/shape/Sector" , function(S , Shape , myMath , Base){
    Base.creatClass(Sector , Shape , {
        draw : function(ctx, context) {
            
-           debugger;
            // 形内半径[0,r)
            var r0 = typeof context.r0 == 'undefined' ? 0 : context.r0;
            var r = context.r;                            // 扇形外半径(0,r]
