a2573163c3bdd3af67c074c8758fc0e99a7555dc
litao.lt <litao.lt@alibaba-inc.com>
2014-02-26 12:43:16 +0800

    del _heart attr

--- build/canvax/index.js
+++ build/canvax/index.js
@@ -1086,7 +1086,7 @@ KISSY.add("canvax/animation/Animation" , function(S){
                 return;
             }
             //说明已经上报心跳 
-            this._heart = true; 
+            //this._heart = true; 
 
            
             //stage存在，才说self代表的display已经被添加到了displayList中，绘图引擎需要知道其改变后
