05d58a42d282cb3d82e23688e5290813dba3f6b4
litao.lt <litao.lt@alibaba-inc.com>
2014-10-13 17:25:30 +0800

    canvax添加对flashcanvas的默认引用

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
                    expand: true,
                    cwd: buildPath,
                    ext: '-min.js',
-                   src: ['**/*.js', '!**/*-min.js'],
+                   src: ['**/*.js', '!**/*-min.js', '!**/flashcanvas.js'],
                    dest: buildPath 
                }
              ]
