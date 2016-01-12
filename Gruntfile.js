56793139f778939e17058b9337787aacd74a81cb
litao.lt <litao.lt@alibaba-inc.com>
2016-01-12 14:21:50 +0800

    rmd convert to commonjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,97 +2,115 @@ module.exports = function(grunt) {
   //配置参数
   var buildPath = "build"
   grunt.initConfig({
-     concat: {
-         options: {
-             separator: ';',
-             stripBanners: true
-         },
-         dist: {
-             src: [
-                 //animationframe
-                 buildPath + "/canvax/animation/AnimationFrame.js",
-                 buildPath + "/canvax/animation/Tween.js",
-                 
-                 //core
-                 buildPath + "/canvax/core/*.js",
-
-                 //CanvaxEvent依赖point
-                 //event
-                 buildPath + "/canvax/display/Point.js",
-                 buildPath + "/canvax/event/CanvaxEvent.js",
-                 buildPath + "/canvax/event/EventManager.js",
-                 buildPath + "/canvax/event/EventDispatcher.js",
-                 buildPath + "/canvax/event/EventHandler.js",
-                 
-                 //geom
-                 buildPath + "/canvax/geom/Math.js",
-                 buildPath + "/canvax/geom/HitTestPoint.js",
-                 buildPath + "/canvax/geom/Matrix.js",
-                 buildPath + "/canvax/geom/Vector.js",
-                 buildPath + "/canvax/geom/SmoothSpline.js",
-                 buildPath + "canvax/geom/bezier.js",
-
-                 //display
-                 buildPath + "/canvax/display/DisplayObject.js",
-                 buildPath + "/canvax/display/DisplayObjectContainer.js",
-                 buildPath + "/canvax/display/Shape.js",
-                 buildPath + "/canvax/display/Sprite.js",
-                 buildPath + "/canvax/display/Stage.js",
-                 buildPath + "/canvax/display/Text.js",
-
-                 //shape中只留下几个最常用的 
-                 buildPath + "/canvax/shape/*.js",
-
-                 "!"+ buildPath +"/canvax/shape/Droplet.js",
-                 "!"+ buildPath +"/canvax/shape/Ellipse.js",
-                 "!"+ buildPath +"/canvax/shape/Isogon.js",
-                 "!"+ buildPath +"/canvax/shape/Shapes.js",
-
-
-                 buildPath + "/canvax/index.js"
-             ],
-             dest: buildPath + "/canvax/index.js"
-         }
-     },
-     uglify: {
-         options: {
-             compress: {
-                 drop_console: true
-             }
-         },
-         dist: {
-             files: [
-               {
-                   expand: true,
-                   cwd: buildPath,
-                   ext: '-min.js',
-                   src: ['**/*.js', '!**/*-min.js'],
-                   dest: buildPath 
-               }
-             ]
-         }
-     },
-     copy: {
-         main: {
-             files: [
-               { expand: true , cwd: "canvax/" , src:"**/*" , dest: buildPath + "/canvax/" }
-             ]
-         }
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true
       },
-      clean: { 
-          build : {
-             src : buildPath
-          }
+      dist: {
+        src: [
+          //animationframe
+          buildPath + "/canvax/animation/AnimationFrame.js",
+          buildPath + "/canvax/animation/Tween.js",
+
+          //core
+          buildPath + "/canvax/core/*.js",
+
+          //CanvaxEvent依赖point
+          //event
+          buildPath + "/canvax/display/Point.js",
+          buildPath + "/canvax/event/CanvaxEvent.js",
+          buildPath + "/canvax/event/EventManager.js",
+          buildPath + "/canvax/event/EventDispatcher.js",
+          buildPath + "/canvax/event/EventHandler.js",
+
+          //geom
+          buildPath + "/canvax/geom/Math.js",
+          buildPath + "/canvax/geom/HitTestPoint.js",
+          buildPath + "/canvax/geom/Matrix.js",
+          buildPath + "/canvax/geom/Vector.js",
+          buildPath + "/canvax/geom/SmoothSpline.js",
+          buildPath + "canvax/geom/bezier.js",
+
+          //display
+          buildPath + "/canvax/display/DisplayObject.js",
+          buildPath + "/canvax/display/DisplayObjectContainer.js",
+          buildPath + "/canvax/display/Shape.js",
+          buildPath + "/canvax/display/Sprite.js",
+          buildPath + "/canvax/display/Stage.js",
+          buildPath + "/canvax/display/Text.js",
+
+          //shape中只留下几个最常用的 
+          buildPath + "/canvax/shape/*.js",
+
+          "!" + buildPath + "/canvax/shape/Droplet.js",
+          "!" + buildPath + "/canvax/shape/Ellipse.js",
+          "!" + buildPath + "/canvax/shape/Isogon.js",
+          "!" + buildPath + "/canvax/shape/Shapes.js",
+
+
+          buildPath + "/canvax/index.js"
+        ],
+        dest: buildPath + "/canvax/index.js"
       }
+    },
+    uglify: {
+      options: {
+        compress: {
+          drop_console: true
+        }
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: buildPath,
+          ext: '-min.js',
+          src: ['**/*.js', '!**/*-min.js'],
+          dest: buildPath
+        }]
+      }
+    },
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: "canvax/",
+          src: "**/*",
+          dest: buildPath + "/canvax/"
+        }]
+      },
+      common : {
+        files: [{
+          expand: true,
+          cwd: "canvax/",
+          src: "**/*",
+          dest: "common/"
+        }]
+      }
+    },
+    clean: {
+      build: {
+        src: buildPath
+      }
+    },
+    r2common: {
+      options: {
+        convertDir : "common",
+        root: "",
+        baseUrl: "",
+        paths: []
+      }
+    }
   });
- 
+
   //载入concat和uglify插件，分别对于合并和压缩
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
- 
-  //注册任务
-  grunt.registerTask( 'default' , [ 'clean' , 'copy' , 'concat' , 'uglify' ] );
-}
 
+  grunt.loadTasks('tasks');
+  //注册任务
+  grunt.registerTask('default', ['clean', 'copy', 'concat', 'uglify']);
+  grunt.registerTask('r2c', ['copy:common' , 'r2common']);
+}
--- tasks/r2common.js
+++ tasks/r2common.js
@@ -2,25 +2,67 @@
  * Copyright (c) 2016 @释剑
  */
 
+ 'use strict';
+
 module.exports = function(grunt) {
-    grunt.registerMultiTask('htmlmin', 'Minify HTML', function() {
-        var options = this.options();
-        grunt.verbose.writeflags(options, 'Options');
+    var fs = require('fs');
+    var helper = require("./helper");
+    //var esprima = require("esprima");
+    //var escodegen = require("escodegen");
+    var r2c = require('require2commonjs');
 
-        this.files.forEach(function(file) {
-            var html = grunt.file.read(file.src) //
-            .replace(/\n+/g, '') //
-            .replace(/\r+/g, '') //
-            .replace(/>\s{2,}</g, '> <') //
-            .replace(/>(.+)</g, function(all, $1) {
-                return '>' + $1.replace(/\s{2,}/g, ' ') + '<'
-            })
-            .replace(/<!\-\-.*?\-\->/g, '') //
+    grunt.registerTask('r2common', 'amd convert to commonjs', function() {
+        var opts = this.options();
+        var root = opts.root;
+        var baseUrl = opts.baseUrl;
+        var paths = opts.paths;
 
-            grunt.file.write(file.dest, html)
-            grunt.log.writeln('File ' + file.dest + ' created.')
+        var convertDir = opts.convertDir;
 
-        });
+        var tarFiles = helper.getAllFiles(convertDir);
+        var viewJsFiles = helper.getTypedFiles(tarFiles, ".js");
+
+        console.log(viewJsFiles.length);
+        return
+
+        for (var i = 0; i < viewJsFiles.length; i++) {
+
+            var jsFilePath = viewJsFiles[i];
+
+            //如果该js文件存在
+            if (fs.existsSync(jsFilePath)) {
+                //计算出来moduleName  
+
+                var moduleName = jsFilePath.replace(convertDir, "").replace(/\/?(.+?)(\.js)?$/, "$1");
+
+                var jsFileContent = fs.readFileSync(jsFilePath, "utf8");
+
+                //先序列化为ast对象( abstract syntax tree )
+                var ast = esprima.parse(jsFileContent, {
+                    attachComment: true
+                });
+
+
+                completeFullModuleFormat(ast, moduleName);
+
+                //操作完后反序列化为字符串
+                var newJsFileContent = escodegen.generate(ast, {
+                    comment: true
+                });
+
+                fs.writeFileSync(jsFilePath, newJsFileContent, "utf8");
+
+
+                //console.log( newJsFileContent );
+
+            }
+        }
+
+        console.log(r2c( "canvax/index.js" , {
+            root : null,
+            baseUrl : null,
+            paths : null
+        }));
 
     });
 };
